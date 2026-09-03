cee7f1f67522a42ad5557799764ab20929f6eb07
Saee <mhsaee@01gmail.com>
2026-09-03 22:14:47 +0530

    Fix port environment import

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 from flask import Flask, request, jsonify
 import joblib
+import os
 
 app = Flask(__name__)
 
 # Load trained model
 model = joblib.load("model.pkl")
 
-
 @app.route("/")
 def home():
     return "ML Model API is running!"
-
 
 @app.route("/predict", methods=["POST"])
 def predict():
@@ -22,10 +21,6 @@ def predict():
         "prediction": int(prediction[0])
     })
 
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
-
-import os
-    
